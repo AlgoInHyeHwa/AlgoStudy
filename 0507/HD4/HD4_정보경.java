cf4e3e8a0fcddee37a3826668d9fa7615f541ea3
BoKyung <bo216@naver.com>
2020-05-08 10:31:02 +0900

    Update HD4_정보경.java

--- 0507/HD4/HD4_정보경.java
+++ 0507/HD4/HD4_정보경.java
@@ -1,1 +1,50 @@
+package test.programmers;
 
+import java.util.*;
+
+public class HDcard_4 {
+	public static int[] solution(int[][] macaron) {
+		int[] result = new int[6];
+		int[][] board = new int[7][7];
+		boolean[][] visit = new boolean[7][7];
+		
+		Queue<Pos> q = new LinkedList<Pos>();
+		
+		for(int i=0; i<macaron.length; i++) {
+			for(int j=board.length-1 ; j>=1 ; j--) {
+				if(board[macaron[i][0]][j]==0) {
+					board[macaron[i][0]][j] = macaron[i][1];
+					
+					q.add(new Pos(macaron[i][0],j));
+					while(!q.isEmpty()) {
+						Pos temp = q.poll();
+						for(int k=0;k<4;k++) {
+							
+						}
+					}
+					
+				}
+			}
+		}
+		
+		return result;
+	}
+	pubilc static void
+	public static class Pos{
+		int x;
+		int y;
+		Pos(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+	}
+	public static void dfs(int[][] board, boolean[][] visit) {
+		
+	}
+	public static void main(String[] args) {
+		int[][] macaron = {{1,1},{2,1},{1,2},{3,3},{6,4},{3,1},{3,3},{3,3},{3,4},{2,1}};
+		
+		solution(macaron);
+
+	}
+}
