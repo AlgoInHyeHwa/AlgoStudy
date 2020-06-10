bf378e9f02e61bb204915bd80b6429fe3ddaae98
Wonsik Kim <46267635+teddyseoul@users.noreply.github.com>
2020-06-11 03:16:06 +0900

    Update 방금그곡_김원식.java

--- 0604/방금그곡/방금그곡_김원식.java
+++ 0604/방금그곡/방금그곡_김원식.java
@@ -2,13 +2,21 @@ public class Pb03 {
     static String solution(String m, String[] musicinfos) {
         String answer = "(NONE)";
         int maxPT = 0;
+//        m = killSharp(m);
+        m = m.replaceAll("A#", "a");
+        m = m.replaceAll("C#", "c");
+        m = m.replaceAll("D#", "d");
+        m = m.replaceAll("F#", "f");
+        m = m.replaceAll("G#", "g");
 
         for(String s : musicinfos) {
-            s = s.replace("A#", "a");
-            s = s.replace("C#", "c");
-            s = s.replace("D#", "d");
-            s = s.replace("F#", "f");
-            s = s.replace("G#", "g");
+//            s = killSharp(m);
+            s = s.replaceAll("A#", "a");
+            s = s.replaceAll("C#", "c");
+            s = s.replaceAll("D#", "d");
+            s = s.replaceAll("F#", "f");
+            s = s.replaceAll("G#", "g");
+
 
             String[] st1 = s.split(",");
             String[] st2 = st1[0].split(":");
@@ -17,8 +25,9 @@ public class Pb03 {
             int playHour = Integer.parseInt(st3[0]) - Integer.parseInt(st2[0]);
             int playMin = Integer.parseInt(st3[1]) - Integer.parseInt(st2[1]) + (playHour * 60);
 
-            String music = makeMusic(st1[3], playMin);
+            String music = makeMusic(st1[3], playMin);  // 시간만큼 재생된 멜로디를 문자열로 반환
 
+            // 멜로디 검사
             if(music.contains(m)) {
                 if (maxPT < playMin) {
                     maxPT = playMin;
@@ -31,6 +40,17 @@ public class Pb03 {
         return answer;
     }
 
+    // 변환
+//    static String killSharp(String s) {
+//        s = s.replaceAll("A#", "a");
+//        s = s.replaceAll("C#", "c");
+//        s = s.replaceAll("D#", "d");
+//        s = s.replaceAll("F#", "f");
+//        s = s.replaceAll("G#", "g");
+//        return s;
+//    }
+
+    // 재생된 멜로디 구간
     static String makeMusic(String s, int t) {
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < t; i++) {
@@ -40,8 +60,10 @@ public class Pb03 {
     }
 
     public static void main(String[] args) {
-        String m = "ABCDEFG";
-        String[] musicinfos = {"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"};
+//        String m = "ABCDEFG";
+//        String[] musicinfos = {"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"};
+        String m = "CC#BCC#BCC#BCC#B";
+        String[] musicinfos = {"03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"};
 
         solution(m, musicinfos);
     }
