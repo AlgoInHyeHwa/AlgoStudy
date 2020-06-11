96422cca86a38a8771cc42ab558a4ebfb9e318df
BoKyung <bo216@naver.com>
2020-06-11 23:34:02 +0900

    Update 방금그곡_정보경.java

--- 0604/방금그곡/방금그곡_정보경.java
+++ 0604/방금그곡/방금그곡_정보경.java
@@ -4,7 +4,14 @@ class Solution {
 
 		String[] music = {};
 
+		m = m.replace("C#", "c");
+		m = m.replace("D#", "d");
+		m = m.replace("F#", "f");
+		m = m.replace("G#", "g");
+		m = m.replace("A#", "a");
+
 		for (String s : musicinfos) {
+
 			music = s.split(",");
 
 			String start[] = music[0].split(":");
@@ -13,26 +20,36 @@ class Solution {
 			String name = music[2];
 			String melody = music[3];
 
+			melody = melody.replace("C#", "c");
+			melody = melody.replace("D#", "d");
+			melody = melody.replace("F#", "f");
+			melody = melody.replace("G#", "g");
+			melody = melody.replace("A#", "a");
+
 			int hour = Integer.parseInt(end[0]) - Integer.parseInt(start[0]);
 			int time = Integer.parseInt(end[1]) - Integer.parseInt(start[1]) + (hour * 60);
 
 			String song = "";
-			
+
 			while (true) {
-				for(int i=0; i<melody.length(); i++) {
-					if(time == song.length()){
+				for (int i = 0; i < melody.length(); i++) {
+					if (time == song.length()) {
 						break;
 					}
 					song += Character.toString(melody.charAt(i));
 				}
-				if(time == song.length()) {
+				if (time == song.length()) {
 					break;
 				}
 			}
-			
-			if(song.contains(melody)) {
+
+			if (song.contains(m)) {
 				answer = name;
 				break;
+			}
+
+			if (answer == "") {
+				answer = "(None)";
 			}
 		}
 
